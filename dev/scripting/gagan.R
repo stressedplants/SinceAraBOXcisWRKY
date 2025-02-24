862099872f00b0d4f6fb0ee13af620257c4ec01a
Gagan Shetteppanavvar <gagan.vishaya@gmail.com>
2025-02-24 01:12:42 +0000

    Week 1 Optional

--- dev/scripting/gagan.R
+++ dev/scripting/gagan.R
@@ -1,5 +1,5 @@
-
 # Week 1 ------------------------------------------------------------------
+
 ## Load data ---------------------------------------------------------------
 
 # Loading libraries
@@ -114,3 +114,113 @@ colours=rainbow(length(unique(clust)))
 plot(gbox.pca.umap$layout[,1], gbox.pca.umap$layout[,2], 
      col=colours[clust[includeCells]], pch=20, 
      main='GSE226097_seedling_12d_230221_PCA', xlab='UMAP Component 1', ylab='UMAP Component 2')
+
+
+
+
+# Week 1 Optional ---------------------------------------------------------
+
+## Load packages and data --------------------------------------------------
+library(Matrix)
+source('dev/utilities/dataprocessingHelperFunctions.R')
+a=load('data/GSE226097_seedling_12d_230221.RData')
+araboxcis=read.csv('data/gboxNetwork22C.csv', header=T)
+
+## Average expression of each gene in each cluster -------------------------
+#make clust not be factors
+clustAsNumbers=as.numeric(paste(clust))
+
+geneExpByCluster = apply(gbox, 1, function(i){
+  sapply(0:(length(unique(clust))-1), function(j){
+    ids=which(clustAsNumbers==j)
+    mean(i[ids])
+  })
+})
+
+colnames(geneExpByCluster)=rownames(gbox)
+dim(geneExpByCluster)
+
+
+## Visualising large tables as heatmaps ------------------------------------
+library('pheatmap')
+
+#pheatmap(geneExpByCluster, scale='column')
+
+clustLabs=read.table('data/clusterLabels.txt', header=T, sep='\t') #read table of cell type in clusters
+
+unique(clustLabs[,'Organ']) # view the names of the dev stage(organs) 
+
+organ='Seedlings_12d' #set the 'organ' name for my data set: Seedlings_12d
+simpleNames=clustLabs[which(clustLabs[,'Organ']==organ), "Cell.type.suggested"] #filter the cell types for the selected stage
+print(simpleNames)
+
+rownames(geneExpByCluster)=simpleNames # set the row names from the filtered list
+
+pheatmap(geneExpByCluster, scale='column') # redo heatmap
+
+write.table(geneExpByCluster, 'data/Seedlingd12_avgExpressionByCluster.txt') # write the table of average expression per cluster
+
+## Focussing on transcription factors --------------------------------------
+tfs=unique(araboxcis[,1]) #Select the unique elements 'from' column with TFs
+tfSubs=tfs[which(tfs %in% rownames(gbox))] #select the tfs present in the gbox dataset
+pheatmap(geneExpByCluster[,tfSubs], scale='column') 
+
+
+## correlation b/w TF expression and  downstream targets -------------------
+corMat=sapply(tfSubs, function(tf){
+  apply(geneExpByCluster, 2, function(gene){
+    cor(geneExpByCluster[,tf], gene, method='spearman')
+  })
+})
+
+dim(corMat)
+
+pheatmap(corMat)
+
+
+## correlation b/w TF and targets per cell ---------------------------------
+id=which(corMat>0.8 & corMat!=1, arr.ind = TRUE)
+dim(id)
+
+dim(corMat)
+
+tVal=apply(id, 1, function(i){
+  row=rownames(corMat)[i[1]]
+  col=colnames(corMat)[i[2]]
+  
+  inBoth=length(which(gbox[row,]>0 & gbox[col,]>0))
+  inNone=length(which(gbox[row,]==0 & gbox[col,]==0))
+  inFirst=length(which(gbox[row,]>0 & gbox[col,]==0))
+  inSecond=length(which(gbox[row,]==0 & gbox[col,]>0))
+  matTemp=matrix(c(inBoth, inFirst, inSecond, inNone), ncol=2)
+  c(inBoth, inFirst, inSecond, inNone, (inBoth*inNone)/(inFirst*inSecond))
+})
+
+hist(log(tVal[5,]), main='Flower', xlab='log odds ratio')
+
+## Comparing between the data sets -----------------------------------------
+
+#lists of genes with positive on cell type level & single cell level
+thresh=exp(1)
+idDoublePositive=which(tVal[5,]>thresh)
+doublePositive=id[idDoublePositive,]
+doublePositive[,1]=rownames(corMat)[doublePositive[,1]]
+doublePositive[,2]=colnames(corMat)[as.numeric(doublePositive[,2])]
+doublePositive=cbind(doublePositive[,2], doublePositive[,1]) #so TF comes before target
+doublePositive=cbind(doublePositive, tVal[5,idDoublePositive])
+
+#save file
+write.table(doublePositive, file='data/seedling-d12_doublePositives.txt', sep='\t', row.names=F)
+
+#list of regulatory pairs that were positive in the first analysis and negative in the second analysis
+thresh=1
+idSimpson=which(tVal[5,]<thresh)
+Simpson=id[idSimpson,]
+
+Simpson[,1]=rownames(corMat)[Simpson[,1]]
+Simpson[,2]=colnames(corMat)[as.numeric(Simpson[,2])]
+Simpson=cbind(Simpson[,2], Simpson[,1]) #so TF comes before target
+Simpson=cbind(Simpson, tVal[idSimpson])
+
+#save file
+write.table(Simpson, file='data/seedling-d12_SimpsonPairs.txt', sep='\t', row.names=F)
