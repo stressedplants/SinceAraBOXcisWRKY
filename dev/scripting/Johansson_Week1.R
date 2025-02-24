6d2082220e8d844f60c019a668e49f9502f56903
MathewJohansson <mathew.johansson1@outlook.com>
2025-02-24 17:59:55 +0000

    Update Johansson_Week1.R

--- dev/scripting/Johansson_Week1.R
+++ dev/scripting/Johansson_Week1.R
@@ -212,11 +212,138 @@ organ='Flower'
 simpleNames = clustLabs[which(clustLabs[,'Organ']==organ), "Cell.type.suggested"]
 print(simpleNames)
 
+# Set row names to those Flower cluster names
+rownames(geneExpByCluster) = simpleNames
+
+# Redo heatmap
+pheatmap(geneExpByCluster, scale = 'column')
+
+# Save underlying table as a file
+write.table(geneExpByCluster, 'data/Flower_avgExpressionByCluster.txt')
+
+
+
+# Focusing on Transcription Factors
+# Same analysis, but focusing on the TFs that bind to perfect G-boxes
+
+# A list of the Transcription Factors 
+tfs = unique(araboxcis[,1])
+
+# Filtered list for only TFs also included in the single-cell RNA-seq dataset
+tfSubs = tfs[which(tfs %in% rownames(gbox))]
+
+# Heatmap of the TFs that bind to perfect G-boxes
+pheatmap(geneExpByCluster[, tfSubs], scale = 'column')
 
 
 
 
+# What is the correlation between expression in TFs and potential downstream targets? 
+# Does the TF regulate downstream gene?
 
+# Correlations between every TF and every potential 
+corMat = sapply(tfSubs, function(tf) {
+  apply(geneExpByCluster, 
+        2,
+        function(gene){
+          cor(geneExpByCluster[,tf], gene, method = 'spearman')
+        })
+})
+
+# Dimensions of correlation testing
+dim(corMat)
+
+# Heatmap of the correlation testing
+pheatmap(corMat)
+
+
+
+# Looking at correlation between TF expression and downstream gene expression in individual cells
+
+# TFs that are highly correlated with downstream genes when looking at cell type level
+id = which(corMat > 0.8 & corMat != 1, arr.ind = TRUE)
+
+# Dimensions of the above
+dim(id)
+
+
+# Neither Pearson's correlation or Spearman's are good choices, because there is a huge amount of 
+#    zero-inflation when calculating correlation at an individual cell level. 
+#    Instead, use the odds ratio of observing both the TF and downstream gene within the same cell.
+
+# Odds ratio = 1 if no. of times two factors occur simultaneously is expected. 
+# Odds ratio >1 if the two factors appear more 'positively' correlated with each other.
+# Odds ratio <1 if the two factors are 'negatively' correlated with each other. 
+
+# The following histogram plots the log-odds ratio. 
+# Log of 1 = 0, log of <1 = negative, log >1 = positive. 
+
+# Dimensions of correlation testing
+dim(corMat)
+
+# Dimensions of the TFs highly correlated with downstream genes
+dim(id)
+
+# Plotting the log-odds ratio
+tVal = apply(id,
+             1,
+             function(i) {
+               row = rownames(corMat)[i[1]]
+               col = colnames(corMat)[i[2]]
+               
+               inBoth = length(which(gbox[row,] > 0 & gbox[col,] > 0))
+               inNone = length(which(gbox[row,] == 0 & gbox[col,] == 0))
+               inFirst = length(which(gbox[row,] > 0 & gbox[col,] == 0))
+               inSecond = length(which(gbox[row,] == 0 & gbox[col,] > 0))
+               
+               matTemp = matrix(c(inBoth, inFirst, inSecond, inNone), ncol = 2)
+               c(inBoth, inFirst, inSecond, inNone, (inBoth * inNone) / (inFirst * inSecond))
+             })
+
+hist(log(tVal[5,]), 
+     main = 'Flower',
+     xlab = 'Log Odds Ratio')
+
+
+
+# Compare between datasets
+
+thresh = exp(1)
+
+idDoublePositive = which(tVal[5,] > thresh)
+doublePositive = id[idDoublePositive,]
+doublePositive[,1] = rownames(corMat)[doublePositive[,2]]
+doublePositive[,2]  = colnames(corMat)[as.numeric(doublePositive[,2])]
+doublePositive = cbind(doublePositive[,2], 
+                       doublePositive[,1]) # TF comes before the target
+doublePositive = cbind(doublePositive,
+                       tVal[5,idDoublePositive])
+
+# Save the file
+write.table(doublePositive, 
+            file = 'data/Flower_doublePositives.txt', 
+            sep = '\t',
+            row.names = F)
+
+# Assembling a set of regulatory pairs that were positive in the first analysis, 
+#    and negative in the second analysis. 
+
+thresh = 1
+idSimpson = which(tVal[5,] < thresh) 
+Simpson = id[idSimpson,1]
+
+Simpson[,1] = rownames(corMat)[Simpson[,1]]
+Simpson[,2] = colnames(corMat)[as.numeric(Simpson[,2])]
+Simpson = cbind(Simpson[,2], 
+                Simpson[,1]) 
+Simpson = cbind(Simpson,
+                tVal[idSimpson])
+
+# Save the file
+write.table(Simpson,
+            file = 'data/Flower_SimpsonPairs.txt',
+            sep = '\t',
+            row.names = F)
 
 
 
